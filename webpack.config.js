473e6e7a62cb18f5a8f8214b372426c909e8d22c
shogo.kitajima <shogo.kitajima@mixi.co.jp>
2022-01-26 16:14:30 +0900

    add bar-chart-race to dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const TerserPlugin = require("terser-webpack-plugin")
 let webpackConfig = {
     mode: 'production',
     entry: {
-        myCustomViz: './src/visualizations/my-custom-viz.ts'
+        "bar-chart-race": './src/visualizations/my-custom-viz.ts'
     },
     output: {
         filename: '[name].js',
